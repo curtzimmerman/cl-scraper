0a519ad49f6d37f2a9d535a8f6a7e7ab514a0d30
Curt Zimmerman <czimmerman9@gmail.com>
2015-08-24 17:01:27 -0400

    refactor out current hits from updating loop to speed up search

--- app/models/search.rb
+++ app/models/search.rb
@@ -21,36 +21,39 @@ class Search < ActiveRecord::Base
 	def refresh
 		#set all previous hits to checked=false
 		Hit.where(search_id: self.id).update_all(checked: false)
+
+		#get array of all ids of hits currently in the database to check against
+		current_hits = self.hits.pluck('data_pid')		
+
 		#update hits for home city
-		self.update_hits(self.city)
+		self.update_hits(self.city, current_hits)		
 
 		#get array of all nearby cities to home city and iterate through them
 		search_cities = self.city.nearby_cities
 		search_cities.each do |city|
-			self.update_hits(city)
+			self.update_hits(city, current_hits)
 		end
 
 		#if the hits were not found in the searches, delete them
 		Hit.where(search_id: self.id, checked: false).delete_all
 	end
 
-	def update_hits(city)
+	def update_hits(city, current_hits)
 		#get the search page using nokogiri
-		doc = Nokogiri::HTML(open(Search.format_query_for_url(city.url, self.category.code, self.parameters_for_search_url)))
+		doc = Nokogiri::HTML(open(Search.format_query_for_url(city.url, self.category.code, self.parameters_for_search_url)))				
 
-		#get array of all ids of hits currently in the database to check against
-		current_hits = self.hits.pluck('data_pid')
 		#iterate over every hit on the page
 		#TODO: add checking for pagination
 		doc.css("div.rightpane div.content p.row").each do |row|
 			if current_hits.nil? || !current_hits.include?(row['data-pid'])
-				self.hits.create(
+				new_hit = self.hits.create(
 					data_pid: row['data-pid'],
 					url: Hit.format_hit_url(city.url, row.css('a')[0]['href']), 
 					title: row.css('a.hdrlnk')[0].text,
 					price: row.css('span.txt span.l2 span.price').text.tr("$", "").to_i,
 					neighborhood: row.css('span.txt span.l2 span.pnr small').text,
 					checked: true)
+				current_hits.push(new_hit.data_pid)
 			elsif
 				#if the document does contain the pid and the current hits listing does also, update the attributes
 				Hit.find_by_data_pid(row['data-pid']).update_attributes(
