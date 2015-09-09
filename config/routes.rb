3983115e38b8fcc6d4ad9b4a7ddb72fb907cb215
Curt Zimmerman <czimmerman9@gmail.com>
2015-09-09 11:38:33 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
   end
 
   get "signup" => "users#new"
-  get "login" => "sessions#new"
-  post "login" => "sessions#create"
   get "help" => "static_pages#help"
   delete "logout" => "sessions#destroy"
 end
