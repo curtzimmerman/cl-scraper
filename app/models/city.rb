920246b63b7f026fa8cf2120b4477f5d44c987ab
Curt Zimmerman <czimmerman9@gmail.com>
2015-12-19 02:39:36 -0500

    fix nearby cities id error

--- app/models/city.rb
+++ app/models/city.rb
@@ -10,11 +10,11 @@ class City < ActiveRecord::Base
 	def get_nearby
 		doc = Nokogiri::HTML(open(self.url))
 		doc.css("div#rightbar ul.menu li.expand.s ul.acitem li.s").each do |nearby|
-			nearby_city_formatted = "http:#{nearby.css('a').first['href']}"
+			nearby_city_formatted = "http:#{nearby.css('a').first['href']}"[0...-1]
 
 			# unless nearby city is in mexico
 			unless nearby_city_formatted.match(/\.mx$/) || nearby_city_formatted.match(/\.ca$/)
-				nearby_city_id = City.find_by_url(nearby_city_formatted).id
+				nearby_city_id = City.where("url like ?", nearby_city_formatted)[0].id
 				self.city_relationships.create(nearby_city_id: nearby_city_id)
 			end
 		end
